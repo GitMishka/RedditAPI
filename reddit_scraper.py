13edf6bf6bfad759fa5839abf0a9ee9e68514d2e
Misha <mishagrigoryan@gmail.com>
2024-03-29 00:04:15 -0400

    brand

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -2,6 +2,7 @@ import praw
 import config
 import pandas as pd
 from datetime import datetime
+from watchBrands import watch_brands
 #credentials from config
 reddit = praw.Reddit(
     client_id=config.reddit_client_id,
@@ -19,7 +20,7 @@ for submission in subreddit.new(limit=None):  # Adjust limit as necessary
     post_details = {
         "id": submission.id,
         "created_utc": datetime.utcfromtimestamp(submission.created_utc).strftime('%Y-%m-%d %H:%M:%S'),  #UTC format
-        
+
         "username": submission.author.name if submission.author else "N/A",
         "num_comments": submission.num_comments,
         "upvotes": submission.score,
@@ -34,5 +35,14 @@ posts_df = pd.DataFrame(posts_data)
 columns_order = [col for col in posts_df.columns if col != 'title'] + ['title']
 posts_df = posts_df[columns_order]
 
+# print(posts_df.head())
+# posts_df.to_csv('watchHistory.csv')
+def find_brand(title):
+    for brand in watch_brands:
+        if brand.lower() in title.lower():
+            return brand
+    return None  # Or "Unknown" if you prefer
+
+# Create a new 'brand' column by applying the find_brand function to each title
+posts_df['brand'] = posts_df['title'].apply(find_brand)
 print(posts_df.head())
-posts_df.to_csv('watchHistory.csv')
---
+++ watchBrands.py
@@ -0,0 +1,12 @@
+watch_brands = [
+    "Rolex", "Omega", "Tag Heuer", "Seiko", "Casio", "Audemars Piguet", "Patek Philippe", "Vacheron Constantin",
+    "IWC Schaffhausen", "Jaeger-LeCoultre", "Panerai", "Breitling", "Hublot", "Breguet", "Cartier", "Tudor",
+    "Longines", "Zenith", "Chopard", "Bell & Ross", "Blancpain", "Girard-Perregaux", "Maurice Lacroix", 
+    "Nomos Glashütte", "Oris", "Rado", "Sinn", "Tissot", "Ulysse Nardin", "F.P. Journe", "Richard Mille", 
+    "A. Lange & Söhne", "Bulova", "Citizen", "Hamilton", "Invicta", "Movado", "Skagen", "Timex", "Victorinox",
+    "Fossil", "Michael Kors", "Emporio Armani", "Montblanc", "Gucci", "Swatch", "Mido", "Frederique Constant",
+    "Baume & Mercier", "Alpina", "Glashütte Original", "Junghans", "Laurent Ferrier", "MB&F", "Piaget", 
+    "Roger Dubuis", "Van Cleef & Arpels", "Arnold & Son", "Bovet", "Czapek", "De Bethune", "H. Moser & Cie",
+    "HYT", "Kudoke", "Louis Moinet", "Meistersinger", "Reservoir", "Romain Jerome", "Sarpaneva", "Urwerk",
+    "Voutilainen", "Waldan", "Zodiac"
+]
