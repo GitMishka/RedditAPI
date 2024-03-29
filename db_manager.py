bd62ab54f9b18eeb9454ed9176d28227457f959e
Misha <mishagrigoryan@gmail.com>
2024-03-29 00:28:58 -0400

    data imported to db

--- db_manager.py
+++ db_manager.py
@@ -1,16 +1,22 @@
-from sqlalchemy import create_engine
-import pandas as pd
+from sqlalchemy import create_engine, text
 from config import pg_host, pg_database, pg_user, pg_password
 from reddit_scraper import posts_df
 
-
-# PostgreSQL connection string using variables from config.py
 connection_string = f"postgresql://{pg_user}:{pg_password}@{pg_host}/{pg_database}"
 
 # Create an SQLAlchemy engine
 engine = create_engine(connection_string)
 
-# Insert the DataFrame into the database
+# SQL command to drop the existing table (if it exists)
+drop_table_command = text("DROP TABLE IF EXISTS reddit_watches;")
+
+# Execute the command to drop the table
+with engine.connect() as connection:
+    connection.execute(drop_table_command)
+    connection.commit()
+
+# Assuming posts_df is your DataFrame containing the new structure
+# This will create a new table with the structure inferred from posts_df
 posts_df.to_sql('reddit_watches', con=engine, index=False, if_exists='replace')
 
 
